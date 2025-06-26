4953eb84e04d75881dbdc9cc17eaca16645605db
manolischrs <manolischrisoulakis@hotmail.com>
2025-06-26 09:46:49 +0300

    changes: rock paper scissors

--- rock paper scissors/index.js
+++ rock paper scissors/index.js
@@ -3,7 +3,7 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const COMPUTER_OPTIONS = ['rock', 'paper', 'scissors'];
+const COMPUTER_OPTIONS = ["rock", "paper", "scissors"];
 
 /* Game functions */
 
@@ -13,34 +13,80 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanSelection = prompt('Pick Rock or Paper or Scissors');
+  let humanSelection = prompt("Pick Rock or Paper or Scissors");
 }
 
-function playRound(humanChoice, computerChoice) {
+function playGame(humanChoice, computerChoice) {
   const formattedUserInput = humanChoice.toLowerCase().trim();
   const rockOption = COMPUTER_OPTIONS[0];
   const paperOption = COMPUTER_OPTIONS[1];
   const scissorsOption = COMPUTER_OPTIONS[2];
 
-  if (formattedUserInput === rockOption && computerChoice === rockOption) {
-    return "It's a draw";
-  } else if (formattedUserInput === rockOption && computerChoice === paperOption) {
-    return 'You lose! Paper beats Rock';
-  } else if (formattedUserInput === rockOption && computerChoice === scissorsOption) {
-    return 'You win! Rock beats Scissors';
-  } else if (formattedUserInput === paperOption && computerChoice === paperOption) {
-    return "It's a draw";
-  } else if (formattedUserInput === paperOption && computerChoice === scissorsOption) {
-    return 'You lose! Scissors beats Paper';
-  } else if (formattedUserInput === paperOption && computerChoice === rockOption) {
-    return 'You win! Paper beats Rock';
-  } else if (formattedUserInput === scissorsOption && computerChoice === scissorsOption) {
-    return "It's a draw";
-    ``;
-  } else if (formattedUserInput === scissorsOption && computerChoice === rockOption) {
-    return ' You lose! Rock beats Paper';
-  } else if (formattedUserInput === scissorsOption && computerChoice === paperOption)
-    return 'You win! Scissors beats Paper';
+  function playRound() {
+    if (formattedUserInput === rockOption && computerChoice === rockOption) {
+      return "It's a draw";
+    } else if (
+      formattedUserInput === rockOption &&
+      computerChoice === paperOption
+    ) {
+      return "You lose! Paper beats Rock";
+    } else if (
+      formattedUserInput === rockOption &&
+      computerChoice === scissorsOption
+    ) {
+      return "You win! Rock beats Scissors";
+    } else if (
+      formattedUserInput === paperOption &&
+      computerChoice === paperOption
+    ) {
+      return "It's a draw";
+    } else if (
+      formattedUserInput === paperOption &&
+      computerChoice === scissorsOption
+    ) {
+      return "You lose! Scissors beats Paper";
+    } else if (
+      formattedUserInput === paperOption &&
+      computerChoice === rockOption
+    ) {
+      return "You win! Paper beats Rock";
+    } else if (
+      formattedUserInput === scissorsOption &&
+      computerChoice === scissorsOption
+    ) {
+      return "It's a draw";
+      ``;
+    } else if (
+      formattedUserInput === scissorsOption &&
+      computerChoice === rockOption
+    ) {
+      return " You lose! Rock beats Paper";
+    } else if (
+      formattedUserInput === scissorsOption &&
+      computerChoice === paperOption
+    )
+      return "You win! Scissors beats Paper";
+  }
+  playRound(5);
+  if (
+    (formattedUserInput === rockOption && computerChoice === paperOption) ||
+    (formattedUserInput === paperOption && computerChoice === scissorsOption) ||
+    (formattedUserInput === scissorsOption && computerChoice === rockOption)
+  ) {
+    return ++computerScore;
+  } else if (
+    (formattedUserInput === rockOption && computerChoice === scissorsOption) ||
+    (formattedUserInput === paperOption && computerChoice === rockOption) ||
+    (formattedUserInput === scissorsOption && computerChoice === paperOption)
+  ) {
+    return ++humanScore;
+  } else if (
+    (formattedUserInput === rockOption && computerChoice === rockOption) ||
+    (formattedUserInput === paperOption && computerChoice === paperOption) ||
+    (formattedUserInput === scissorsOption && computerChoice === scissorsOption)
+  ) {
+    return ++humanScore && ++computerScore;
+  }
 }
 const humanSelection = getHumanChoice();
 const computerSelections = getComputerChoice();
