5ad283ffb9112ef6503d9dd0d0e2266d124d3cf4
manolischrs <manolischrisoulakis@hotmail.com>
2025-07-02 11:00:10 +0300

    added: ui to rock paper scissors

--- rock paper scissors/index.js
+++ rock paper scissors/index.js
@@ -3,7 +3,7 @@
 let humanScore = 0;
 let computerScore = 0;
 
-const COMPUTER_OPTIONS = ['rock', 'paper', 'scissors'];
+const COMPUTER_OPTIONS = ["rock", "paper", "scissors"];
 const MAX_SCORE = 5;
 
 /* Game functions */
@@ -13,16 +13,21 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  return prompt('Pick Rock or Paper or Scissors');
+  return prompt("Pick Rock or Paper or Scissors");
 }
 
 function announceWinner() {
   if (humanScore === MAX_SCORE) {
-    return 'You win the game!';
+    return "You win the game!";
   } else if (computerScore === MAX_SCORE) {
-    return 'Computer wins the game!';
+    return "Computer wins the game!";
   }
 }
+const rockButton = document
+  .getElementById("Rock")
+  .addEventListener("click", playGame);
+const paperButton = document.getElementById("Paper");
+const scissorsButton = document.getElementById("Scissors");
 
 /* 
 1. get both choices
@@ -38,22 +43,24 @@ function playGame() {
   const humanChoice = getHumanChoice();
   const computerChoice = getComputerChoice();
 
-  const formattedUserInput = humanChoice.toLowerCase().trim();
+  const formattedUserInput = [rockButton, paperButton, scissorsButton];
   const rockOption = COMPUTER_OPTIONS[0];
   const paperOption = COMPUTER_OPTIONS[1];
   const scissorsOption = COMPUTER_OPTIONS[2];
 
   function handleScoreUpdate() {
     if (
-      (formattedUserInput === rockOption && computerChoice === paperOption) ||
-      (formattedUserInput === paperOption && computerChoice === scissorsOption) ||
-      (formattedUserInput === scissorsOption && computerChoice === rockOption)
+      (formattedUserInput === rockButton && computerChoice === paperOption) ||
+      (formattedUserInput === paperButton &&
+        computerChoice === scissorsOption) ||
+      (formattedUserInput === scissorsButton && computerChoice === rockOption)
     ) {
       computerScore += 1;
     } else if (
-      (formattedUserInput === rockOption && computerChoice === scissorsOption) ||
-      (formattedUserInput === paperOption && computerChoice === rockOption) ||
-      (formattedUserInput === scissorsOption && computerChoice === paperOption)
+      (formattedUserInput === rockButton &&
+        computerChoice === scissorsOption) ||
+      (formattedUserInput === paperButton && computerChoice === rockOption) ||
+      (formattedUserInput === scissorsButton && computerChoice === paperOption)
     ) {
       humanScore += 1;
     }
@@ -62,41 +69,54 @@ function playGame() {
   function playRound() {
     handleScoreUpdate();
 
-    if (formattedUserInput === rockOption && computerChoice === rockOption) {
+    if (formattedUserInput === rockButton && computerChoice === rockOption) {
       return "It's a draw";
-    } else if (formattedUserInput === rockOption && computerChoice === paperOption) {
-      return 'You lose! Paper beats Rock';
-    } else if (formattedUserInput === rockOption && computerChoice === scissorsOption) {
-      return 'You win! Rock beats Scissors';
-    } else if (formattedUserInput === paperOption && computerChoice === paperOption) {
+    } else if (
+      formattedUserInput === rockButton &&
+      computerChoice === paperOption
+    ) {
+      return "You lose! Paper beats Rock";
+    } else if (
+      formattedUserInput === rockButton &&
+      computerChoice === scissorsOption
+    ) {
+      return "You win! Rock beats Scissors";
+    } else if (
+      formattedUserInput === paperButton &&
+      computerChoice === paperOption
+    ) {
       return "It's a draw";
-    } else if (formattedUserInput === paperOption && computerChoice === scissorsOption) {
-      return 'You lose! Scissors beats Paper';
-    } else if (formattedUserInput === paperOption && computerChoice === rockOption) {
-      return 'You win! Paper beats Rock';
-    } else if (formattedUserInput === scissorsOption && computerChoice === scissorsOption) {
+    } else if (
+      formattedUserInput === paperButton &&
+      computerChoice === scissorsOption
+    ) {
+      return "You lose! Scissors beats Paper";
+    } else if (
+      formattedUserInput === paperButton &&
+      computerChoice === rockOption
+    ) {
+      return "You win! Paper beats Rock";
+    } else if (
+      formattedUserInput === scissorsButton &&
+      computerChoice === scissorsOption
+    ) {
       return "It's a draw";
       ``;
-    } else if (formattedUserInput === scissorsOption && computerChoice === rockOption) {
-      return ' You lose! Rock beats Paper';
-    } else if (formattedUserInput === scissorsOption && computerChoice === paperOption) {
-      return 'You win! Scissors beats Paper';
+    } else if (
+      formattedUserInput === scissorsButton &&
+      computerChoice === rockOption
+    ) {
+      return " You lose! Rock beats Paper";
+    } else if (
+      formattedUserInput === scissorsButton &&
+      computerChoice === paperOption
+    ) {
+      return "You win! Scissors beats Paper";
     } else {
-      return 'Invalid choice! Please pick Rock, Paper, or Scissors.';
+      return "Invalid choice! Please pick Rock, Paper, or Scissors.";
     }
   }
-
   const resultMessage = playRound();
   console.log(resultMessage);
   console.log(`Scoreboard: Human ${humanScore} - Computer ${computerScore}`);
-}
-
-while (humanScore < MAX_SCORE || computerScore < MAX_SCORE) {
-  playGame();
-  const finalMessage = announceWinner();
-
-  if (finalMessage) {
-    console.log(finalMessage);
-    break;
-  }
 }
